20ed3a73f991bbb1d8a6f3adac684e9661d48b0d
SatkarNiraula <satkarniraula69@gmail.com>
2025-04-11 15:52:31 +0545

    minor changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,5 @@
 import axios from "axios";
-const API_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://taskmanagerassesback-production.up.railway.app/api";
-
+const API_URL = process.env.REACT_APP_API_URL;
 const api = axios.create({
   baseURL: API_URL,
   headers: {
